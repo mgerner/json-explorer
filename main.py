2023dc1e3ef4c28203cf96675749b29fd41d0c89
Martin <martin@Martins-MacBook-Pro.local>
2017-05-11 00:34:38 +0100

    #1 add a filtering Entry box to the main app

--- main.py
+++ main.py
@@ -45,25 +45,68 @@ def _filter(data, keys):
         return res
 
 class MainApp(Tk):
+    mappings = None
+    cache = None
+    filtered_data = None
+
     def __init__(self, data):
         Tk.__init__(self, None)
 
+        # create first layout level (a frame and a text element)
         self.grid()
 
-        self.tree = ttk.Treeview(self)
-        self.tree.bind('<<TreeviewSelect>>', self.selected)
-        self.mappings = _add_items(self.tree, data)
-        self.cache = {}
+        f = Frame(self)
 
         self.text = Text(self)
         self.text.config(state=DISABLED)
 
-        self.tree.grid(row=0, column=0, sticky=NSEW)
+        f.grid(row=0, column=0, sticky=NSEW)
         self.text.grid(row=0, column=1, sticky=NSEW)
-
         self.grid_columnconfigure(0, weight=0)
         self.grid_columnconfigure(1, weight=1)
         self.grid_rowconfigure(0, weight=1)
+
+        # create the layout of the frame
+        self.tree = ttk.Treeview(f)
+        self.tree.bind('<<TreeviewSelect>>', self.selected)
+
+        self.filter_box = Entry(f)
+        self.filter_box.bind('<Return>', self.filter)
+        self.filter_box.pack()
+
+        self.filter_box.grid(row=0, column=0, sticky=NSEW)
+        self.tree.grid(row=1, column=0, sticky=NSEW)
+        f.grid_columnconfigure(0, weight=1)
+        f.grid_rowconfigure(0, weight=0)
+        f.grid_rowconfigure(1, weight=1)
+
+        # set the data that should be used, and trigger the population of the controls
+        self.original_data = data
+        self.filter(None)
+
+
+    def filter(self, _event):
+        if not self.filter_box.get().strip():
+            self.filtered_data = self.original_data
+            self.populate_views()
+            return
+
+        keys = self.filter_box.get().split(',')
+        keys = [k.strip() for k in keys if k.strip()]
+
+        self.filtered_data = _filter(self.original_data, keys)
+        self.populate_views()
+
+    def populate_views(self):
+        if self.tree.exists('root'):
+            self.tree.delete('root')
+
+        self.tree.insert('', 'end', 'root', text='root', open=True)
+
+        self.mappings = _add_items(self.tree, self.filtered_data, 'root')
+        self.cache = {}
+
+        self.tree.selection_set('root')
 
     def selected(self, _event):
         selected = self.tree.selection()[0]
@@ -74,6 +117,13 @@ class MainApp(Tk):
         self.text.delete(1.0, END)
         self.text.insert(END, self.cache[selected])
         self.text.config(state=DISABLED)
+
+    def run(self):
+        self.focus_force()
+        self.lift()
+        self.attributes('-topmost', True)
+        self.after_idle(self.attributes, '-topmost', False)
+        self.mainloop()
 
 class ReceiveDataApp(Tk):
     def __init__(self):
@@ -105,7 +155,7 @@ if __name__ == '__main__':
         if len(sys.argv) > 2:
             data = _filter(data, sys.argv[2].split(','))
 
-        MainApp(data).mainloop()
+        MainApp(data).run()
 
     else:
         ReceiveDataApp().mainloop()
